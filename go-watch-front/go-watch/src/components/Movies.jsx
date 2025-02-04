ad7b0582dc076057472ec964a02d5cbad23c0978
Guilherme Weber <gh63388@gmail.com>
2025-02-04 20:25:17 -0300

    feat: create fake API

--- go-watch-front/go-watch/src/components/Movies.jsx
+++ go-watch-front/go-watch/src/components/Movies.jsx
@@ -1,4 +1,43 @@
+import { useState, useEffect } from 'react'
+
 export default function Movies() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    let movieList = [
+      {
+        id: 1,
+        title: 'The Shawshank Redemption',
+        release_date: '1994-09-14',
+        runtime: 142,
+        genre: 'Drama',
+        mpaa_rating: 'R',
+        description: 'Two imprisoned',
+      },
+      {
+        id: 2,
+        title: 'The Godfather',
+        release_date: '1972-03-24',
+        runtime: 175,
+        genre: 'Crime, Drama',
+        mpaa_rating: 'R',
+        description:
+          'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+      },
+      {
+        id: 3,
+        title: 'The Dark Knight',
+        release_date: '2008-07-18',
+        runtime: 152,
+        genre: 'Action, Crime, Drama',
+        mpaa_rating: 'PG-13',
+        description:
+          'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.',
+      },
+    ]
+    setMovies(movieList)
+  }, [])
+
   return (
     <>
       <div className="text-center">
