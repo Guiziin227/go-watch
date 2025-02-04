856593c6237ca4318261ac76a9e08c92794b5070
Guilherme Weber <gh63388@gmail.com>
2025-02-04 15:31:25 -0300

    feat: add errorElement

--- go-watch-front/go-watch/src/main.jsx
+++ go-watch-front/go-watch/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { App } from './App.jsx'
+import { Error } from './components/Error.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
   },
 ])
 
