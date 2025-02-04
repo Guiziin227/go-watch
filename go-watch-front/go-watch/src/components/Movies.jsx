5985b6b8a972acec308d19f53fe4582dacd39023
Guilherme Weber <gh63388@gmail.com>
2025-02-04 20:32:42 -0300

    feat: crate the visible table of movies

--- go-watch-front/go-watch/src/components/Movies.jsx
+++ go-watch-front/go-watch/src/components/Movies.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Movies() {
   const [movies, setMovies] = useState([])
@@ -43,6 +44,28 @@ export default function Movies() {
       <div className="text-center">
         <h2>Movies!</h2>
         <hr />
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>Movie</th>
+              <th>Release Date</th>
+              <th>Runtime</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((m) => (
+              <tr key={m.id}>
+                <td>
+                  <Link to={`/movies/</td>${m.id}`}>{m.title}</Link>
+                </td>
+                <td>{m.release_date}</td>
+                <td>{m.runtime}</td>
+                <td>{m.mpaa_rating}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   )
