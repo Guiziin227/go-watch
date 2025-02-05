8dd6571cefb504ec6c6d0aaef9bb6f6b26e5ceab
Guilherme Weber <gh63388@gmail.com>
2025-02-05 14:47:07 -0300

    feat: create backend base

---
+++ go-watch-back/cmd/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	//set app config
+	var app application
+
+	//read the command line arguments
+
+	//connect to the database
+
+	app.Domain = "example.com"
+
+	
+	//start the server
+	log.Printf("Starting server on port %d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error starting the server: ", err)
+		log.Fatal(err)
+	}
+}
