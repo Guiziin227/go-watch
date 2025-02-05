6cdc94e844eb1e901465689785fab327576273ec
Guilherme Weber <gh63388@gmail.com>
2025-02-04 21:36:24 -0300

    feat: login page

--- go-watch-front/go-watch/src/components/Login.jsx
+++ go-watch-front/go-watch/src/components/Login.jsx
@@ -1,8 +1,44 @@
+import { useState } from 'react'
+import Input from './form/Input'
+
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="text-center">
+    <div className="col-md-6 offset-md-3">
       <h2>Login!</h2>
       <hr />
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email"
+          type="email"
+          className="form-control"
+          name="email"
+          autoComplete="email-new"
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          autoComplete="password-new"
+          placeholder="Enter password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <hr />
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
     </div>
   )
 }
