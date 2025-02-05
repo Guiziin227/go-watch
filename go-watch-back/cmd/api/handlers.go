8a379a458e3669a8c9bc8eb8f458c7df9c50f971
Guilherme Weber <gh63388@gmail.com>
2025-02-05 18:59:26 -0300

    feat: add fake api for test

--- go-watch-back/cmd/api/handlers.go
+++ go-watch-back/cmd/api/handlers.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! From %s", app.Domain)
+	var payload = struct{
+		Status string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status: "active",
+		Message: "Welcome to the Go Watch API",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Error marshalling the response: ", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+
 }
