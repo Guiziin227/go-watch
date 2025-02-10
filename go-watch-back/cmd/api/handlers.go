68d114b7e28f40b2a895cf6493d4de8df48df4f0
Guilherme Weber <gh63388@gmail.com>
2025-02-10 15:08:02 -0300

    feat: create a handler authenticate

--- go-watch-back/cmd/api/handlers.go
+++ go-watch-back/cmd/api/handlers.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	var payload = struct{
-		Status string `json:"status"`
+	var payload = struct {
+		Status  string `json:"status"`
 		Message string `json:"message"`
 		Version string `json:"version"`
 	}{
-		Status: "active",
+		Status:  "active",
 		Message: "Welcome to the Go Watch API",
 		Version: "1.0.0",
 	}
@@ -19,11 +20,33 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-		movies, err := app.DB.AllMovies() // Chama o método AllMovies do repositório para obter todos os filmes
-		if err != nil {
-			app.errorJSON(w, err)
-			return
-		}
+	movies, err := app.DB.AllMovies() // Chama o método AllMovies do repositório para obter todos os filmes
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
 	_ = app.writeJSON(w, http.StatusOK, movies)
+}
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	//read json payload
+
+	//validate user against database
+
+	//check password
+
+	//create a jwt user
+	u := jwtUser{ID: 1, FirstName: "John", LastName: "Doe"}
+
+	//generate tokens
+	tokens, err := app.auth.GenerateTokenPair(&u)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println(tokens.Token)
+
+	w.Write([]byte(tokens.Token))
 }
