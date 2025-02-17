c2f323d3f18222063b67efd8c582dedcc519e32b
Guilherme Weber <gh63388@gmail.com>
2025-02-17 17:27:37 -0300

    proptype

--- go-watch-front/go-watch/src/components/form/TextArea.jsx
+++ go-watch-front/go-watch/src/components/form/TextArea.jsx
@@ -1,17 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 export function TextArea(props) {
+    const { name, title, value, onChange, errorDiv, errorMsg } = props;
 
-    const {name,title,value, onChange, errorDiv, errorMsg} = props
-
-    return(
+    return (
         <div className="mb-3">
-
             <label htmlFor={name} className="form-label">
                 {title}
             </label>
-
-            <textarea className="form-control" id={name} name={name}cols="30" rows="10" value={value} onChange={onChange}/>
+            <textarea
+                className="form-control"
+                id={name}
+                name={name}
+                cols="30"
+                rows="10"
+                value={value}
+                onChange={onChange}
+            />
             <div className={errorDiv}>{errorMsg}</div>
         </div>
-    )
-
+    );
 }
+
+TextArea.propTypes = {
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    errorDiv: PropTypes.string,
+    errorMsg: PropTypes.string,
+};
+
+TextArea.defaultProps = {
+    errorDiv: '',
+    errorMsg: '',
+};
