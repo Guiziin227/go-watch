fd1145b0359c28eb26b764010acc7b6b62d25c30
Guilherme Weber <gh63388@gmail.com>
2025-02-26 18:15:45 -0300

    Create route and function Allgenres

--- go-watch-back/internal/repository/repository.go
+++ go-watch-back/internal/repository/repository.go
@@ -13,4 +13,6 @@ type DatabaseRepo interface {
 
 	OneMovie(id int64) (*models.Movie, error)
 	OneMovieForEdit(id int64) (*models.Movie, []*models.Genre, error)
+
+	AllGenres() ([]*models.Genre, error)
 }
