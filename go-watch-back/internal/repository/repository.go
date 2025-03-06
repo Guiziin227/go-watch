e15e919a9eceb38af772f03b8a48e3e04f22af78
Guilherme Weber <gh63388@gmail.com>
2025-03-06 18:37:40 -0300

    Delete movie, handler-route-dbrepo

--- go-watch-back/internal/repository/repository.go
+++ go-watch-back/internal/repository/repository.go
@@ -10,10 +10,13 @@ type DatabaseRepo interface {
 	AllMovies() ([]*models.Movie, error)
 	OneMovie(id int64) (*models.Movie, error)
 	OneMovieForEdit(id int64) (*models.Movie, []*models.Genre, error)
-	InsertMovie(movie models.Movie) (int, error)
-	UpdateMovieGenres(id int, genreIDs []int) error
+
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
 	AllGenres() ([]*models.Genre, error)
+
+	InsertMovie(movie models.Movie) (int, error)
+	UpdateMovieGenres(id int, genreIDs []int) error
 	UpdateMovie(movie models.Movie) error
+	DeleteMovie(id int) error
 }
