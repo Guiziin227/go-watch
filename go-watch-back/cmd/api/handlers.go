7c535d5ddfda1776473fa263518a6e26ad6adcac
Guilherme Weber <gh63388@gmail.com>
2025-03-09 15:23:49 -0300

    create the handler

--- go-watch-back/cmd/api/handlers.go
+++ go-watch-back/cmd/api/handlers.go
@@ -347,3 +347,19 @@ func (app *application) DeleteMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJSON(w, http.StatusAccepted, resp)
 }
+
+func (app *application) AllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	genreID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	movies, err := app.DB.AllMovies(genreID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movies)
+}
